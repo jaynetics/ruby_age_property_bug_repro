058431b80dba19b9148d9afc588c56e15d2edb91
Janosch Müller <janosch.mueller@betterplace.org>
2022-04-08 20:07:57 +0200

    Experiment 3

--- repro.rb
+++ repro.rb
@@ -15,10 +15,10 @@ assert_support_prop('ascii')
 refute_support_prop('non-existent-prop')
 assert_support_prop('age=10.0')
 refute_support_prop('age=15.0')
-200.downto(16).each { |n| refute_support_prop("age=#{n}.0") }
 
 if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('3.2.0')
   refute_support_prop('age=14.0') # <-- unexpected behavior on Ruby 2.x-3.0
 else
+  999.downto(16).each { |n| refute_support_prop("age=#{n}.0") }
   assert_support_prop('age=14.0')
 end

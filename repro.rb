7643be9662e749315a29ee05a87d46b09390b65d
Janosch Müller <janosch.mueller@betterplace.org>
2022-04-08 15:42:12 +0200

    Add repro

---
+++ repro.rb
@@ -0,0 +1,23 @@
+def support_prop?(prop)
+  Regexp.new("/\\p{#{prop}}/") rescue false
+end
+
+def assert_support_prop(prop)
+  support_prop?(prop) || raise("ruby #{RUBY_VERSION} should support #{prop}")
+end
+
+def refute_support_prop(prop)
+  support_prop?(prop) && raise("ruby #{RUBY_VERSION} should NOT support #{prop}")
+end
+
+# some sanity checks
+assert_support_prop('ascii')
+refute_support_prop('non-existent-prop')
+assert_support_prop('age=10.0')
+refute_support_prop('age=15.0')
+
+if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('3.2.0')
+  refute_support_prop('age=14.0') # <-- unexpected behavior on Ruby 2.x-3.0
+else
+  assert_support_prop('age=14.0')
+end
